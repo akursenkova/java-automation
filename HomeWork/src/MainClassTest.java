0fa530c176441e607caa492c589b9d37c0920e24
Angelica Kursenkova <akursenkova@gmail.com>
2021-04-24 21:09:23 +0300

    1 урок. Тест 2

--- HomeWork/src/MainClassTest.java
+++ HomeWork/src/MainClassTest.java
@@ -4,13 +4,21 @@ public class MainClassTest extends MainClass {
 
     @Test
 
-    public void testGetLocalNumber() {
+    /* public void testGetLocalNumber() {
 
         int a = this.getLocalNumber();
         if (a == 14) {
             System.out.println("True");
         } else {
             System.out.println("Number 'a' doesn't equals 14");
+        }
+    } */
+
+    public void testGetClassNumber() {
+        if (getClassNumber() > 45) {
+            System.out.println("True");
+        } else {
+            System.out.println("Your number is less than 45");
         }
     }
 }
